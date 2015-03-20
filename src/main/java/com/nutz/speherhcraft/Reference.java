68f6679dae216f1d9b92b847de8a6bde5c2bba9a
Spencer <spencie01@gmail.com>
2015-03-20 12:52:47 -0400

    woo stuff

---
+++ src/main/java/com/nutz/speherhcraft/Reference.java
@@ -0,0 +1,7 @@
+package com.nutz.speherhcraft;
+
+public class Reference {
+	public static final String MOD_ID = "speherhcraft";
+	public static final String MOD_NAME = "Le Speherhcraft Mod";
+	public static final String VERSION = "0.1a";
+}
